14d671da1f3ab494030171bb575a9a6460030e77
NidhiManjrekar3 <141571541+NidhiManjrekar3@users.noreply.github.com>
2023-08-24 11:55:32 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousal from '../components/Carousal'
+export default function Home() {
+  return (
+    <div>
+       <div> <Navbar/></div>
+       <div><Carousal/></div>
+       <div>
+        <Card/>
+       </div>
+       <div><Footer/></div>
+      
+    </div>
+  )
+}
